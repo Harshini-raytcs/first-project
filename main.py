5e95013e96908901e4b6308aa18838bf5c9d591f
harshini <harshini.dataengineering@gmail.com>
2024-07-25 15:28:00 -0400

    commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+import boto3
+
+def create_bucket(bucket_name, region=None):
+    """Create an S3 bucket in a specified region"""
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+        print(f"Bucket {bucket_name} created.")
+    except Exception as e:
+        print(f"Error creating bucket: {e}")
+
+if _name_ == "_main_":
+    create_bucket("your-new-bucket-name", "us-east-1")
